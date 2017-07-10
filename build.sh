705a198aa17e5f215879454f050ca8011f4d1dd0
eabeda <elyes.abeda@gmail.com>
2017-07-10 15:25:10 +0200

    Add make linux-menuconfig and make uboot-menuconfig

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 cd buildroot/
 make qemu_arm_versatile_defconfig
 make menuconfig
+#make linux-menuconfig
+#make uboot-menuconfig
 make
 qemu-system-arm -M versatilepb -kernel output/images/zImage -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic
 
