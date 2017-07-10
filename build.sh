d712ff78e5457c17c13cd63098abbdd62fa6bddd
eabeda <elyes.abeda@gmail.com>
2017-07-10 15:30:16 +0200

    Fix adding make targets

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ cd buildroot/
 make qemu_arm_versatile_defconfig
 make menuconfig
 #make linux-menuconfig
-#make uboot-menuconfig
+#make busybox-menuconfig
+#make help
 make
 qemu-system-arm -M versatilepb -kernel output/images/zImage -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic
 
