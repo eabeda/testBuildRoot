24ccf2ba3619ebe6ebce7e4dea89992c5ff578e7
eabeda <elyes.abeda@gmail.com>
2017-07-10 15:22:28 +0200

    Refix build.sh script

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ make menuconfig
 make
 qemu-system-arm -M versatilepb -kernel output/images/zImage -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic
 
-#Modules (already integrated in zImage)
-cd output/build/linux-4.11.3
-mkinitramfs -o initrd.img-4.11.3
-cd -
-qemu-system-arm -M versatilepb -kernel output/images/zImage -initrd output/build/linux-4.11.3/initrd.img-4.11.3 -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic
-ls /lib/modules/4.11.3/kernel/fs/fat/
+# #Modules (already integrated in zImage)
+# cd output/build/linux-4.11.3
+# mkinitramfs -o initrd.img-4.11.3
+# cd -
+# qemu-system-arm -M versatilepb -kernel output/images/zImage -initrd output/build/linux-4.11.3/initrd.img-4.11.3 -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic
+# ls /lib/modules/4.11.3/kernel/fs/fat/
 
-#Network
-qemu-system-arm -M versatilepb -kernel output/images/zImage -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -netdev user,id=eth0 -device e1000,netdev=eth0,mac=64:00:6a:88:1e:2a -nographic
+# #Network
+# qemu-system-arm -M versatilepb -kernel output/images/zImage -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -netdev user,id=eth0 -device e1000,netdev=eth0,mac=64:00:6a:88:1e:2a -nographic
